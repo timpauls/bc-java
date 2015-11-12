870a638206ee8d80231fdf94713d7f32666ed4e6
Tim Pauls <tim.pauls.hh@gmail.com>
2015-11-12 17:53:30 +0100

    Remove already completed TODO for SRA KeyFactory

--- prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/SRA.java
+++ prov/src/main/java/org/bouncycastle/jcajce/provider/asymmetric/SRA.java
@@ -1,17 +1,8 @@
 package org.bouncycastle.jcajce.provider.asymmetric;
 
-import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
-import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
 import org.bouncycastle.jcajce.provider.config.ConfigurableProvider;
 import org.bouncycastle.jcajce.provider.util.AsymmetricAlgorithmProvider;
 
-// TODO: SRA KeyFactory
-// research:
-// the bc BaseKeyFactorySpi class does many things we appearantly won't need for sra imo.
-// it's maybe more clever to just extend the KeyFactorySpi.class from java.security
-// that's all we need to fulfill the requirements as far as i can see.
-// first thought: all we need for this, are SRAKeySpecs for private and public key.
-// rest should be given, since we use RSA as a black box.
 public class SRA
 {
     private static final String PREFIX = "org.bouncycastle.jcajce.provider.asymmetric" + ".sra.";
